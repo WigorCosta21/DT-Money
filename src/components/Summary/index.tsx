832ac16dd6f36f64f23fa665c3b9ab244d4ecf9e
Wigor <wigor21@hotmail.com>
2025-01-27 22:04:00 -0300

    feat: create summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import * as S from "./styles";
+
+export const Summary = () => {
+  return (
+    <S.SummaryContainer>
+      <S.SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </S.SummaryCard>
+      <S.SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>R$ R$ 1.259,00</strong>
+      </S.SummaryCard>
+      <S.SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#FFF" />
+        </header>
+
+        <strong>R$ 16.141,00</strong>
+      </S.SummaryCard>{" "}
+    </S.SummaryContainer>
+  );
+};
