956ca48678ed1907d443ea9cc9463c3a2a98fdd6
Wigor <wigor21@hotmail.com>
2025-01-27 22:16:57 -0300

    feat: create transactions component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/globals";
+import { Transactions } from "./pages/Transactions";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Mt Money</h1>
+      <Transactions />
     </ThemeProvider>
   );
 };
